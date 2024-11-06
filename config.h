d02fc6c3d449c2bffd00f91df98118e9e96a836c
wuyns <a0_23e@163.com>
2024-11-06 13:09:26 +0800

    使用xfce4-screenshooter作为截图工具

--- config.h
+++ config.h
@@ -132,11 +132,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *xfce4screenshooter[] = { "xfce4-screenshooter", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,				        XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_e,      spawn,          {.v = xfce4screenshooter }},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_b,      toggleextrabar, {0} },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
